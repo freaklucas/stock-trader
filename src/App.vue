26d30fada7087408f58ba291dd6a6c7fd6ed03bd
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-06 17:06:59 -0300

    router-view ok

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <v-app>
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
