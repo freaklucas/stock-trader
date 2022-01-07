0b11fd487fb9ed747e558b734326be717dd295c1
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-07 17:05:29 -0300

    arrays hardcode ok

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,22 @@
 <template>
-  <h1>Ações</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" />
+  </v-layout>
 </template>
 
 <script>
-export default {};
+import Stock from "./Stock.vue";
+export default {
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "MBW", price: 1000 },
+        { id: 2, name: "Google", price: 100 },
+        { id: 3, name: "facebook", price: 200 },
+        { id: 4, name: "Twitter", price: 800 },
+      ],
+    };
+  },
+};
 </script>
-
-<style>
-</style>
