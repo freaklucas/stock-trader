5210e7ad55be67ec3959fd55e3e3a00b0aa2db8a
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-10 11:39:39 -0300

    config text

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,7 +1,12 @@
 <template>
-  <v-layout row wrap mt-2>
-    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
-  </v-layout>
+  <div>
+    <h1 class="text-no-wrap blue bold" style="width: 9rem">
+      Venda ações quando valorizar :)
+    </h1>
+    <v-layout row wrap mt-2>
+      <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+    </v-layout>
+  </div>
 </template>
 
 <script>
