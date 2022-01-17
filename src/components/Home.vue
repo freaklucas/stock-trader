ad274cf031089d4d8d5fc291e659612994a16ca2
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-17 13:00:12 -0300

    exibindo valor na home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,31 @@
 <template>
-  <h1>Início</h1>
+  <div>
+    <h1 class="display-3 font-weight-light mb-4">
+      Negocie e Consulte suas Ações
+    </h1>
+    <v-sheet :elevation="12" color="primary" class="pa-3">
+      <v-icon dark right class="mr-5 mb-1"> mdi-checkbox-marked-circle</v-icon>
+      <span class="headline white--text font-weight-light"
+        >Você pode Salvar & Carregar os Dados
+      </span>
+    </v-sheet>
+    <v-sheet :elevation="12" color="success" class="pa-3 group mt-3 darken-1">
+      <v-icon dark right class="mr-5 mb-1"> mdi-checkbox-marked-circle </v-icon>
+      <span class="headline white--text font-weight-light"
+        >Clique em 'Finalizar Dia' para iniciar um novo dia!
+      </span>
+    </v-sheet>
+    <v-divider class="my-4" />
+    <p class="display-1"><strong>Seu Saldo:</strong>{{ credit }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    credit() {
+      return this.$store.getters.credit;
+    },
+  },
+};
 </script>
-
-<style>
-</style>
