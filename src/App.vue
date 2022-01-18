fc9979b344e9c6fc0a57ec1f48d1558baef03452
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-18 16:53:44 -0300

    adicionando animaçao

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <Header />
     <v-main class="grey lighten-2">
       <v-container>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </v-container>
     </v-main>
   </v-app>
@@ -22,3 +24,34 @@ export default {
   },
 };
 </script>
+
+<style>
+@keyframes slide-in {
+  from {
+    transform: translateY(-30);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 0.3s ease;
+}
+.slide-leave-active {
+  animation: slide-out 0.3s ease;
+}
+</style>
